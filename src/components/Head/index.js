b41d62e2369a1ab215843af8e5c8c0bff56c0c7d
Taslim A Hussain <taslim@outlook.co.nz>
2020-05-26 17:46:01 +1200

    update

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Head = () => (
     <React.Fragment>
-        <h1>Mastering D3 Tree Layout</h1>
+        <h1 style={{
+            textAlign: "center"
+        }}>Mastering D3 Tree Layout</h1>
     </React.Fragment>
 );
 
--- src/components/Tree/index.js
+++ src/components/Tree/index.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef} from 'react';
-import {hierarchy, tree, select} from 'd3';
+import {hierarchy, tree, select, linkHorizontal, zoom, event} from 'd3';
 import data from '../../data/data.json';
 import './index.scss';
 
@@ -14,10 +14,8 @@ const Tree = React.forwardRef((props, canvasRef) => {
      * Links are object that contains *Source and *Target nodes (They are use to draw the linking lines on the tree or force graph)
      * Returns a root node (which has bunch of useful methods attached)
      */
-    const root = hierarchy(data);
-
-    const treeLayout = tree(root);
-
+    let root = hierarchy(data);
+    
     
     // Component did mount
     useEffect(() => {
@@ -29,13 +27,53 @@ const Tree = React.forwardRef((props, canvasRef) => {
         // Get the height of the canvas
         const height = Math.floor(canvas.node().getBoundingClientRect().height);
 
-
+        const svgWidth = width - 20;
+        const svgHeight = height -20;
+        
         // Create svg
         const svg = select(svgRef.current);
-        svg.node().getB
-                svg.attr('width', width - 20)
-                    .attr('height', height - 20)
+                svg.attr('width', svgWidth)
+                    .attr('height', svgHeight)
                     .attr('class', 'svg');
+
+        /** ****************************************************************************************** */
+
+        // Generate tree layout with the default
+        const treeLayout = tree().size([svgHeight - 30, svgWidth - 100]);   
+        
+        // Generate Links
+        const links = treeLayout(root).links();
+
+        // Horizontal link path generator
+        const genHorizontalLink = linkHorizontal().x(d => d.y).y(d => d.x);
+        
+        // Group paths
+        const gPath = svg.append('g').attr('transform', `translate(70, 15)`);
+
+        // Add Zoom
+        svg.call(zoom().on('zoom', () => {
+            gPath.attr('transform', event.transform);
+        }));
+        
+        // Render links
+        gPath.selectAll('path')
+            .data(links)
+            .enter()
+            .append('path')
+            .attr('d', genHorizontalLink);
+
+
+        // Render text
+        gPath.selectAll('text')
+            .data(root.descendants())
+            .enter()
+            .append('text')
+            .attr('x', d => d.y)
+            .attr('y', d => d.x)
+            .attr('dy', '0.32em')
+            .attr('text-anchor', d => d.children ? 'middle' : 'start')
+            .attr('font-size', d => 3.25 - d.depth + 'em')
+            .text(d => d.data.data.id);
     }, []);
 
 

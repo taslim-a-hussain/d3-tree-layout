e6e322af05c5b8823433823ce0b1c77aa9c7c807
Taslim A Hussain <taslim@outlook.co.nz>
2020-05-27 10:09:24 +1200

    update

--- src/components/Svg/index.js
+++ src/components/Svg/index.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, {useRef, useEffect, useState} from 'react';
+import {select} from 'd3';
+import './index.scss';
 
 const Svg = () => {
 
+    // Ref
+    const canvasRef = useRef();
+
+
+    // Component did mount
+    useEffect(() => {
+        const canvas = select(canvasRef.current);
+        
+        // Create svg element on the canvas
+        const svg = canvas.append('svg').attr('transform', `translate(60, 60)`);
+            svg.attr('class', 'circle-svg');
+
+        const g = svg.append('g');
+    
+
+        // Create circle
+        const circle = g.append('circle');
+            circle.attr('r', 50)
+                  .attr('cx', 51)
+                  .attr('cy', 51)
+                  .attr('fill', 'none')
+                  .attr('stroke', 'orange')
+                  .attr('stroke-width', '2');
+        
+
+        // Write text inside the circle
+        const text = g.append('text');
+            text.text('nocraming')
+            text.attr('x', '50%')
+            text.attr('y', '50%')
+            text.attr('text-anchor', 'middle')
+            text.attr('dy', '.3em');
+
+
+    }, []);
+
     return (
-        <div>
-            <h1>Svg</h1>
+        <div className="svg-playground-container">
+            <div ref={canvasRef}></div>
         </div>
     );
 };
